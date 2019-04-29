f407bda70ed1b03492d28ac35f3b01ecb4848714
CVitor <carlos.vitor.caetano@gmail.com>
2019-04-29 15:39:11 -0300

    Criação da page em html e css

--- porrinha/views.py
+++ porrinha/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def quemSomos(request):
+    return render(request, 'quemSomos.html')
--- qualquerPorra/urls.py
+++ qualquerPorra/urls.py
@@ -19,5 +19,6 @@ from porrinha import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index),
+    path('sobre/', views.quemSomos),
 ]
